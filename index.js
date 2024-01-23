5edcaae6c4fa2563348c3b9743143756a513bb7f
Be-bo <robfiker@gmail.com>
2024-01-22 17:52:05 -0800

    Added initial test code for mongodb integration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const {MongoClient} = require('mongodb');
 const accountRoutes = require('./routes/account');
 const playgroundRoutes = require('./routes/playground');
 require('dotenv').config();
@@ -9,6 +10,41 @@ const {PORT} = process.env;
 app.use(express.json());
 app.use(cors());
 app.use('/public', express.static('public'));
+
+
+// MARK: MongoDB Testing
+const url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1'; // Replace with your MongoDB server URL
+const dbName = 'testDatabase'; // Replace with your database name
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB');
+    const db = client.db(dbName);
+
+    // You can perform database operations here
+
+    // Example: Insert a document into a collection
+    const collection = db.collection('users'); // Replace with your collection name
+    const document = { name: 'John', age: 30 };
+    return collection.insertOne(document);
+  })
+  .then((result) => {
+    console.log('Inserted document:', result);
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  })
+  .finally(() => {
+    // Close the MongoDB client when done
+    client.close();
+  });
+
+  // LEFT OFF: realized I'll need to migrate databases to the Ubuntu server
+  // will have to do that and then test saving to mongo on there
+  // then I can move to authentication integration
+
+
 
 
 // MARK: Test Routes
