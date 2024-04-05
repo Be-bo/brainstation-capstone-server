7351c1d02f5a7a490e1be5404f77982d16f492af
Be-bo <robfiker@gmail.com>
2024-04-05 00:52:26 -0700

    Added more prints

--- routes/playground.js
+++ routes/playground.js
@@ -50,7 +50,8 @@ router.post('/playground/generate', upload.single('face_image'), async (req, res
             const categoryCollectionName = categoryDocument['name'] + '_category';
             console.log(categoryCollectionName);
             const categoryCollection = client.db().collection(categoryCollectionName);
-            const itemName = await categoryCollection.findOne({ _id: new ObjectId(categoriesObject[i]['selected_clothing_id']) });
+            console.log(categoriesObject[i]['selected_clothing_id']);
+            const itemName = await categoryCollection.findOne({ _id: ObjectId(categoriesObject[i]['selected_clothing_id']) });
             const colorName = categoriesObject[i]['selected_color'][0];
             clothingProperties += ' ' + colorName + ' ' + itemName;
         }
