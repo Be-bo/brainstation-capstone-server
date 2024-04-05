f8917430a3d299e7fef15254b953196bda6227b3
Be-bo <robfiker@gmail.com>
2024-04-04 21:43:47 -0700

    Added multer set up and initial code for testing face image uploads

--- routes/playground.js
+++ routes/playground.js
@@ -3,37 +3,59 @@ const express = require('express');
 const router = express.Router();
 const OpenAI = require("openai");
 const helpers = require('../helpers');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
-const {OPENAI_API_KEY} = process.env;
+const { OPENAI_API_KEY } = process.env;
 const historyJsonPath = 'generationHistory.json';
-const {MongoClient, ObjectId} = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const mongoUri = 'mongodb://localhost:27017/toga_database';
+
+// Set up multer to handle file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, 'public', 'faces')); // Save uploads to the 'public/faces' folder
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname); // Keep the original file name
+    }
+});
+const upload = multer({ storage: storage });
 
 
 // MARK: Create a Generation Item
 // http://3.20.237.64:80/playground/generate -- UBUNTU SERVER
 // http://3.145.198.110:80/playground/generate -- AWS LINUX SERVER
-router.post('/playground/generate', async (req, res) => {
+router.post('/playground/generate', upload.single('face_image'), async (req, res) => {
+
+    // Handle file upload
+    if (!req.file) {
+        return res.status(400).send('No file uploaded');
+    }
+    res.status(200).send('File uploaded successfully');
+
+
     try {
-        const prompt = helpers.constructPrompt(req.body);
-        console.log('Starting generation with prompt: ', prompt);
-        const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
-        const response = await openai.images.generate({ model: "dall-e-3", prompt: prompt, n: 1, size: "1024x1024", });
-        console.log('Successfully processed a generation request, OpenAI response: ', response);
+        // const prompt = helpers.constructPrompt(req.body);
+        // console.log('Starting generation with prompt: ', prompt);
+        // const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+        // const response = await openai.images.generate({ model: "dall-e-3", prompt: prompt, n: 1, size: "1024x1024", });
+        // console.log('Successfully processed a generation request, OpenAI response: ', response);
 
-        const itemId = uuidv4();
-        const imageName = itemId + '.png';
-        console.log(imageName);
-        const savedImageUrl = 'http://3.145.198.110:80/public/' + imageName;
-        console.log(savedImageUrl);
-        const savedImagePath = await helpers.saveImageFromURL(response.data[0].url, '../public/'+imageName);
-        console.log('img saved successfully: ', savedImagePath);
+        // const itemId = uuidv4();
+        // const imageName = itemId + '.png';
+        // console.log(imageName);
+        // const savedImageUrl = 'http://3.145.198.110:80/public/' + imageName;
+        // console.log(savedImageUrl);
+        // const savedImagePath = await helpers.saveImageFromURL(response.data[0].url, '../public/'+imageName);
+        // console.log('img saved successfully: ', savedImagePath);
 
-        const newGenerationItem = helpers.constructGenerationItem(req.body, [savedImageUrl], itemId);
-        await helpers.saveItemToGenerationHistory(req.body.userId, newGenerationItem, historyJsonPath);
-        console.log('Successfully saved item to local json: ', newGenerationItem);
-        res.status(201).json(newGenerationItem);
+        // const newGenerationItem = helpers.constructGenerationItem(req.body, [savedImageUrl], itemId);
+        // await helpers.saveItemToGenerationHistory(req.body.userId, newGenerationItem, historyJsonPath);
+        // console.log('Successfully saved item to local json: ', newGenerationItem);
+        // res.status(201).json(newGenerationItem);
 
     } catch (error) {
         console.log('Failed to create a new playground generation item: ', error);
@@ -44,9 +66,9 @@ router.post('/playground/generate', async (req, res) => {
 
 
 // MARK: Get Base Information For All Clothing Categories
-router.get('/playground/clothing-categories', async(req,res) => {
-    try{
-        const client = new MongoClient(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true});
+router.get('/playground/clothing-categories', async (req, res) => {
+    try {
+        const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
         await client.connect();
 
         const categoriesCollection = client.db().collection('clothing_categories');
@@ -55,7 +77,7 @@ router.get('/playground/clothing-categories', async(req,res) => {
         await client.close();
         res.status(201).json(documents);
 
-    }catch(e){
+    } catch (e) {
         console.error('Error: ', e);
         res.status(500).send('Server failed to get clothing categories collection.');
     }
@@ -63,26 +85,24 @@ router.get('/playground/clothing-categories', async(req,res) => {
 
 
 // MARK: Get All Items of a Specific Clothing Category
-router.get('/playground/category', async(req, res) =>{
+router.get('/playground/category', async (req, res) => {
     const categoryId = req.query.categoryId;
 
-    console.log(req.query);
-
-    try{
-        const client = new MongoClient(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true});
+    try {
+        const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
         await client.connect();
 
         const categoriesCollection = client.db().collection('clothing_categories');
-        const categoryDocument = await categoriesCollection.findOne({_id: ObjectId(categoryId)});
+        const categoryDocument = await categoriesCollection.findOne({ _id: ObjectId(categoryId) });
 
-        const categoryCollectionName = categoryDocument['name']+'_category';
+        const categoryCollectionName = categoryDocument['name'] + '_category';
         const categoryCollection = client.db().collection(categoryCollectionName);
         const documents = await categoryCollection.find({}).toArray();
 
         await client.close();
         res.status(201).json(documents);
-        
-    }catch(e){
+
+    } catch (e) {
         console.error('Error: ', e);
         res.status(500).send('Server failed to get items for category with id: ' + categoryId);
     }
