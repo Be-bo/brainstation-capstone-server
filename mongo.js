f653706def78087e8c73d1a236ff060d5d0d6646
Be-bo <robfiker@gmail.com>
2024-04-08 06:53:46 -0700

    Added async func

--- mongo.js
+++ mongo.js
@@ -1,19 +1,21 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const mongoUri = 'mongodb://localhost:27017/toga_database';
 
-const newAddress = 'http://3.130.188.16:80/';
-const oldAddress = 'http://3.20.237.64:80/';
+async function main() {
+    const newAddress = 'http://3.130.188.16:80/';
+    const oldAddress = 'http://3.20.237.64:80/';
 
-const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
-await client.connect();
-const targetCollection = client.db().collection('bottom_category');
+    const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const targetCollection = client.db().collection('bottom_category');
 
-const result = await targetCollection.updateMany(
-    { image: { $exists: true } }, // Filter documents with the "image" field
-    [
-        { $set: { image: { $concat: [newAddress, { $substr: ['$image', 22, -1] }] } } }
-    ]
-);
+    const result = await targetCollection.updateMany(
+        { image: { $exists: true } }, // Filter documents with the "image" field
+        [
+            { $set: { image: { $concat: [newAddress, { $substr: ['$image', 22, -1] }] } } }
+        ]
+    );
 
-console.log(result);
-await client.close();
+    console.log(result);
+    await client.close();
+}
