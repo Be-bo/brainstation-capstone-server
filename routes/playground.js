6cd1e43a161626bfe7cf7f2aba660f0f8cf15d85
Be-bo <robfiker@gmail.com>
2024-04-07 21:02:30 -0700

    Fixed id keys

--- routes/playground.js
+++ routes/playground.js
@@ -146,7 +146,7 @@ async function combineClothingProperties(clothingCategories, clientCategoriesSel
     let clothingProperties = '';
     console.log(clothingCategories);
     for (let i = 0; i < clientCategoriesSelections.length; i++) {
-        const categoryDocument = clothingCategories.find(obj => obj['category_id'] === clientCategoriesSelections[i]['category_id']); // need category name first to know which collection to target for clothing item name
+        const categoryDocument = clothingCategories.find(obj => obj['_id'] === ObjectId(clientCategoriesSelections[i]['category_id'])); // need category name first to know which collection to target for clothing item name
         console.log(categoryDocument);
         // const categoryDocument = await clothingCategories.findOne({ _id: ObjectId(clientCategoriesSelections[i]['category_id']) });
         const categoryCollectionName = categoryDocument['name'] + '_category';
