7d69f9519e95ec8def41a8990306a7d3956f5a5f
Be-bo <robfiker@gmail.com>
2024-04-08 09:24:08 -0700

    CORS policy headers

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use(express.json());
 app.use(cors());
 app.use('/public', express.static('public'));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 
 // MARK: Test Routes
 app.get('/', (req, res) => {
