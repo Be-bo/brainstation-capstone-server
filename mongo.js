618f63e633ae0b12a5a6f4d5311515c149cdafa7
Be-bo <robfiker@gmail.com>
2024-04-08 06:51:34 -0700

    Script to update ip addresses

---
+++ mongo.js
@@ -0,0 +1,19 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const mongoUri = 'mongodb://localhost:27017/toga_database';
+
+const newAddress = 'http://3.130.188.16:80/';
+const oldAddress = 'http://3.20.237.64:80/';
+
+const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+await client.connect();
+const targetCollection = client.db().collection('bottom_category');
+
+const result = await targetCollection.updateMany(
+    { image: { $exists: true } }, // Filter documents with the "image" field
+    [
+        { $set: { image: { $concat: [newAddress, { $substr: ['$image', 22, -1] }] } } }
+    ]
+);
+
+console.log(result);
+await client.close();
