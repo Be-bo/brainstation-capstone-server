1aa657957f6a65bfffd08570268507e3f9b36f1f
Be-bo <robfiker@gmail.com>
2024-04-07 21:21:56 -0700

    Removed test prints

--- routes/playground.js
+++ routes/playground.js
@@ -144,11 +144,8 @@ async function combineClothingProperties(clothingCategories, clientCategoriesSel
     const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
     let clothingProperties = '';
-    console.log(clothingCategories);
-    console.log(clientCategoriesSelections);
     for (let i = 0; i < clientCategoriesSelections.length; i++) {
         const categoryDocument = clothingCategories.find(obj => obj['_id'] === clientCategoriesSelections[i]['category_id']); // need category name first to know which collection to target for clothing item name
-        console.log(categoryDocument);
         // const categoryDocument = await clothingCategories.findOne({ _id: ObjectId(clientCategoriesSelections[i]['category_id']) });
         const categoryCollectionName = categoryDocument['name'] + '_category';
         const categoryCollection = client.db().collection(categoryCollectionName);
